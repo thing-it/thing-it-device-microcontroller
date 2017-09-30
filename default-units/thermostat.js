9929e0715b0c75b071ada6fd37def0cbff3f561d
klausberberich <klaus@thing-it.com>
2017-09-30 16:37:50 +0200

    Thermostat actor initial commit.

---
+++ default-units/thermostat.js
@@ -0,0 +1,360 @@
+module.exports = {
+    metadata: {
+        plugin: "thermostat",
+        label: "Microcontroller Thermostat",
+        role: "actor",
+        family: "thermostat",
+        deviceTypes: ["microcontroller/microcontroller"],
+        services: [
+            {id: "incrementSetpoint", label: "Increment Setpoint"},
+            {id: "decrementSetpoint", label: "Decrement Setpoint"},
+        ],
+        state: [
+            {
+                id: "setpoint", label: "Setpoint",
+                type: {
+                    id: "decimal"
+                }
+            }, {
+                id: "temperature", label: "Temperature",
+                type: {
+                    id: "decimal"
+                }
+            }, {
+                id: "mode", label: "Mode",
+                type: {
+                    id: "string"
+                }
+            }, {
+                id: "heatActive", label: "Heat Active",
+                type: {
+                    id: "boolean"
+                }
+            }, {
+                id: "coolActive", label: "Cool Active",
+                type: {
+                    id: "boolean"
+                }
+            }],
+        configuration: [
+            {
+                label: "Minimum Setpoint",
+                id: "minimumSetpoint",
+                type: {
+                    id: "integer"
+                },
+                defaultValue: 18
+            }, {
+                label: "Maximum Setpoint",
+                id: "maximumSetpoint",
+                type: {
+                    id: "integer"
+                },
+                defaultValue: 26
+            }, {
+                label: "Tolerance",
+                id: "tolerance",
+                type: {
+                    id: "integer"
+                },
+                defaultValue: 1
+            }, {
+                label: "Step Size",
+                id: "stepSize",
+                type: {
+                    id: "integer"
+                },
+                defaultValue: 1
+            }
+            // TODO Julian - add configuration for temp sensor, display, increase button, decrease button, mode LEDs
+        ]
+    },
+    create: function () {
+        return new Thermostat();
+    }
+};
+
+var q = require('q');
+
+/**
+ *
+ */
+function Thermostat() {
+    /**
+     *
+     */
+    Thermostat.prototype.start = function () {
+        var tempRangeMidpoint = (this.configuration.minimumSetpoint + this.configuration.maximumSetpoint) / 2;
+
+        this.state = {
+            setpoint: tempRangeMidpoint
+        };
+
+        if (this.isSimulated()) {
+            this.logDebug("Starting in simulated mode");
+
+            this.simulationInterval = setInterval(function () {
+                this.update()
+            }.bind(this), 10000);
+        } else {
+            this.logDebug("Starting in non-simulated mode");
+        }
+
+        return this.update();
+    };
+
+    /**
+     *
+     */
+    Thermostat.prototype.stop = function () {
+        var promise;
+
+        if (this.isSimulated()) {
+            if (this.simulationInterval) {
+                clearInterval(this.simulationInterval);
+            }
+
+            promise = q();
+        } else {
+            // TODO Julian - muss hier was aufgeraumt werden?
+        }
+
+        return promise;
+    };
+
+    /**
+     *
+     */
+    Thermostat.prototype.getState = function () {
+        return this.state;
+    };
+
+    /**
+     *
+     */
+    Thermostat.prototype.setState = function (targetstate) {
+        var promise = q();
+        this.logDebug('Received set state.', targetstate);
+
+        if (targetstate) {
+            if (targetstate.temperature) {
+                this.state.temperature = targetstate.temperature;
+            }
+
+            if (targetstate.setpoint) {
+                this.state.setpoint = targetstate.setpoint;
+            }
+
+            promise = this.determineMode()
+                .then(function () {
+                    this.publishStateChange();
+                    this.logDebug('State set.');
+                }.bind(this));
+
+        } else {
+            promise = q.fcall(function () {
+                throw new Error('Provided state was empty.')
+            })
+        }
+
+        return promise;
+    };
+
+    /**
+     *
+     */
+    Thermostat.prototype.update = function () {
+        this.logDebug('Updating values.');
+        var promise = this.getTemperatureFromSensor()
+            .then(function () {
+                this.logDebug('Got temperature.');
+                return this.determineMode();
+            }.bind(this))
+            .then(function () {
+                this.publishStateChange();
+                this.logDebug('Values updated.');
+            }.bind(this))
+            .fail(function (error){
+                this.logError(error);
+                return q();
+            }.bind(this));
+
+        return promise;
+    };
+
+
+    /**
+     *
+     */
+    Thermostat.prototype.modifySetpoint = function (targetModification) {
+        var promise;
+        this.logDebug('Modifying setpoint.', targetModification);
+
+        this.state.setpoint += targetModification;
+
+        promise = this.determineMode()
+            .then(function () {
+                this.publishStateChange();
+                this.logDebug('Setpoint modified.');
+            }.bind(this));
+
+        return promise;
+    };
+
+    /**
+     * TODO Julian - connect the button for increasing the setpoint to this method.
+     */
+    Thermostat.prototype.incrementSetpoint = function () {
+        var promise;
+
+        if ((this.state.setpoint + this.configuration.stepSize) <= this.configuration.maximumSetpoint) {
+            promise = this.modifySetpoint(this.configuration.stepSize);
+        } else {
+            this.logDebug('Ignoring attempt to increase setpoint over limit.');
+            promise = q();
+        }
+
+        return promise;
+    };
+
+    /**
+     * TODO Julian - connect the button for decreasing the setpoint to this method.
+     */
+    Thermostat.prototype.decrementSetpoint = function () {
+        var promise;
+
+        if ((this.state.setpoint - this.configuration.stepSize) >= this.configuration.minimumSetpoint) {
+            promise = this.modifySetpoint(this.configuration.stepSize * -1);
+        } else {
+            this.logDebug('Ignoring attempt to decrease setpoint below limit.');
+            promise = q();
+        }
+
+        return promise;
+    };
+
+    /**
+     *
+     */
+    Thermostat.prototype.getTemperatureFromSensor = function () {
+        var deferred = q.defer();
+
+        if (this.isSimulated()) {
+            switch (this.state.mode) {
+                case 'HEAT':
+                    this.state.temperature += Math.random();
+                    break;
+                case 'COOL':
+                    this.state.temperature -= Math.random();
+                    break;
+                case 'NEUTRAL':
+                    //do nothing;
+                    break;
+                default:
+                    this.state.temperature = 20;
+            }
+
+            deferred.resolve;
+        } else {
+            this.state.temperature = 20; // TODO Julian - implement actual sensor update call instead
+            deferred.resolve;
+        }
+
+        this.logDebug('Temperature retrieved.', this.state.temperature);
+        this.logDebug(deferred.promise);
+        return deferred.promise;
+    };
+
+
+    /**
+     *
+     */
+    Thermostat.prototype.determineMode = function () {
+        this.logDebug('Determining mode.', this.state.setpoint, this.state.temperature)
+        var promise;
+        var delta = this.state.setpoint - this.state.temperature;
+
+        if (Math.abs(delta) > this.configuration.tolerance) {
+            if (delta > 0) {
+                this.state.heatActive = true;
+                this.state.coolActive = false;
+                promise = this.setMode('HEAT');
+            } else {
+                this.state.heatActive = fale;
+                this.state.coolActive = true;
+                promise = this.setMode('COOL');
+            }
+        } else {
+            this.state.heatActive = false;
+            this.state.coolActive = false;
+            promise = this.setMode('NEUTRAL');
+        }
+
+        return promise.then(function(){
+            return this.updateDisplay();
+        }.bind(this));
+    };
+
+    /**
+     *
+     * @param mode
+     */
+    Thermostat.prototype.setMode = function (mode) {
+        var color;
+
+        this.state.mode = mode;
+
+        switch (mode) {
+            // TODO Julian - replace the color names with what you actually want to send to the LED.
+            case 'HEAT':
+                color = 'red';
+                break;
+            case 'COOL':
+                color = 'blue';
+                break;
+            case 'NEUTRAL':
+                color = 'white';
+                break;
+            default:
+                this.logError('Got unexpected mode: "' + mode + '". Valid modes are HEAT, COOL, and NEUTRAL.');
+        }
+
+        return this.updateLEDs(color);
+    };
+
+    /**
+     *
+     * @param color
+     */
+    Thermostat.prototype.updateLEDs = function (color) {
+        this.logDebug('Setting LED color.', color);
+        var deferred = q.defer();
+
+        if (this.isSimulated()) {
+            deferred.resolve;
+        } else {
+            // TODO Julian - implement actual LED update call
+            deferred.resolve;
+        }
+
+        return deferred.promise;
+    };
+
+
+    /**
+     *
+     */
+    Thermostat.prototype.updateDisplay = function () {
+        this.logDebug('Setting display.', this.state.temperature, this.state.setpoint);
+        var deferred = q.defer();
+
+        if (this.isSimulated()) {
+            deferred.resolve;
+        } else {
+            // TODO Julian - implement actual Display update call
+            deferred.resolve;
+        }
+
+        return deferred.promise;
+    };
+}
---
+++ test/pluginTestThermostat.js
@@ -0,0 +1,123 @@
+var q = require('q');
+var assert = require("assert");
+
+
+describe('[thing-it] BACnet Device', function () {
+    var testDriver;
+    var initialState;
+    var lastState;
+
+    before(function () {
+        testDriver = require("thing-it-test").createTestDriver({logLevel: "debug"});
+
+        testDriver.registerDevicePlugin(__dirname + "/../microcontroller");
+        testDriver.registerUnitPlugin(__dirname + "/../default-units/thermostat");
+        testDriver.start({
+            configuration: require("../examples/configurationThermostat.js"),
+            simulated: true,
+            heartbeat: 10
+        });
+
+    });
+
+    describe('Thermostat', function () {
+        describe('#start', function () {
+            this.timeout(5000);
+
+            it('should have an initial value for setpoint, temperature, and mode',
+                function (done) {
+                    setTimeout(function () {
+                        var currentState = testDriver.microcontroller.thermostat.getState();
+                        console.log('***');
+                        console.log(currentState);
+                        console.log('***');
+                        initialState = JSON.parse(JSON.stringify(currentState));
+
+                        try {
+                            assert.notEqual(initialState.setpoint, undefined, 'setpoint undefined');
+                            assert.notEqual(initialState.temperature, undefined, 'temperature undefined');
+                            assert.notEqual(initialState.mode, undefined, 'mode undefined');
+                            done();
+                        } catch (err) {
+                            console.log('ERROR DEBUG pluginTestThermostat: Initial state after 5s.', initialState);
+                            done(err);
+                        }
+                    }, 4000);
+                });
+        });
+
+        describe('#incrementSetpoint', function () {
+            it('should increase the setpoint modification and the setpoint by 1',
+                function (done) {
+                    var desiredState = JSON.parse(JSON.stringify(initialState));
+                    desiredState.setpoint += 1;
+
+                    testDriver.microcontroller.thermostat.incrementSetpoint()
+                        .then(function () {
+                            var resultingState = testDriver.microcontroller.thermostat.getState();
+                            lastState = resultingState;
+                            assert.equal(resultingState.setpoint, desiredState.setpoint);
+                            done();
+                        })
+                        .fail(function (error) {
+                            done(error);
+                        });
+                });
+        });
+
+        describe('#decrementSetpoint twice', function () {
+            this.timeout(5000);
+
+            it('should decrease the setpoint modification and the setpoint by 2',
+                function (done) {
+                    console.log('**************');
+                    console.log(lastState);
+                    console.log('**************');
+                    var desiredState = JSON.parse(JSON.stringify(lastState));
+                    desiredState.setpoint -= 2;
+
+                    testDriver.microcontroller.thermostat.decrementSetpoint()
+                        .delay(1000)
+                        .then(function () {
+                            return testDriver.microcontroller.thermostat.decrementSetpoint();
+                        })
+                        .delay(1000)
+                        .then(function () {
+                            var resultingState = testDriver.microcontroller.thermostat.getState();
+                            assert.equal(resultingState.setpoint, desiredState.setpoint);
+                            lastState = resultingState;
+                            done();
+                        })
+                        .fail(function (error) {
+                            done(error);
+                        });
+                });
+        });
+
+
+        describe('#setState', function () {
+            this.timeout(4000);
+
+            it('should set the device back to the initial state',
+                function (done) {
+                    var desiredState = initialState;
+
+                    testDriver.microcontroller.thermostat.setState(desiredState)
+                        .then(function () {
+                            var resultingState = testDriver.microcontroller.thermostat.getState();
+                            assert.equal(resultingState.setpoint, desiredState.setpoint);
+                            assert.equal(resultingState.temperature, desiredState.temperature);
+                            done();
+                        })
+                        .fail(function (error) {
+                            done(error);
+                        });
+                });
+        });
+    })
+
+
+    after(function () {
+        testDriver.stop();
+    });
+});

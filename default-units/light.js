3d4ebc99174d1543af4ce280d461a60f23d67d6f
julianseibert <julianseibert@gmx.de>
2018-01-26 23:19:16 +0100

    implement light sensor

--- default-units/light.js
+++ default-units/light.js
@@ -6,21 +6,43 @@ module.exports = {
         family: "light",
         deviceTypes: ["microcontroller/microcontroller"],
         unit: "LUX",
-        configuration: [
-            {
-                label: "Controller",
-                id: "controller",
-                type: {
-                    family: "enumeration",
-                    values: [{
-                        label: "BH1750  (I2C)",
-                        id: "BH1750"
-                    }, {
-                        label: "TSL2561 (I2C)",
-                        id: "TSL2561"
-                    }]
-                }
-            }]
+        state: [{
+            id: "luminance",
+            label: "Luminance",
+            type: {
+                id: "number"
+            }
+        }],
+        configuration: [{
+            label: "Controller",
+            id: "controller",
+            type: {
+                family: "enumeration",
+                values: [{
+                    label: "BH1750  (I2C)",
+                    id: "BH1750"
+                }, {
+                    label: "TSL2561 (I2C)",
+                    id: "TSL2561"
+                }]
+            }
+        }, {
+            label: "Threshold",
+            id: "threshold",
+            type: {
+                id: "number"
+            },
+            unit: "LUX",
+            defaultValue: 10,
+        }, {
+            label: "Average Time",
+            id: "averageTime",
+            type: {
+                id: "number"
+            },
+            unit: "sec",
+            defaultValue: 30,
+        }]
     },
     create: function () {
         return new LightSensor();
@@ -37,23 +59,41 @@ function LightSensor() {
     LightSensor.prototype.start = function () {
         try {
             if (!this.isSimulated()) {
+
+                this.logLevel = 'debug';
+
                 var five = require("johnny-five");
 
+                this.state = {
+                    luminance: 0
+                };
+
                 this.lightSensor = new five.Light({
-                    freq: this.configuration.rate,
                     controller: this.configuration.controller,
+                    freq: 500,
                 });
 
+                var value = [];
                 var self = this;
-
-                this.lightSensor.on("change", function (event) {
-
-
-                });
 
                 this.lightSensor.on("data", function (data) {
 
+                    value.push(data.lux);
 
+                    if (value.length > self.configuration.averageTime * 2) {
+                        value.shift();
+                        let sum = value.reduce(function (a, b) {
+                            return (a + b)
+                        });
+
+                        let average = (sum / value.length).toFixed(2);
+
+                        if ((Math.abs(average - self.state.luminance)) >= self.configuration.threshold) {
+                            self.state.luminance = average;
+                            self.logDebug("\x1b[36mLuminance: \x1b[0m" + self.state.luminance);
+                            self.publishStateChange();
+                        }
+                    }
                 });
             }
         } catch (x) {
@@ -61,4 +101,10 @@ function LightSensor() {
                 + this.id + ":" + x);
         }
     };
-};
+    /**
+     *
+     */
+    LightSensor.prototype.getState = function () {
+        return this.state;
+    };
+}
